774e89e6d3ba47a801b1708c4e6176d3e09f3e4f
Schyler Hosler <schylerhosler@gmail.com>
2016-07-22 16:19:13 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # your code goes here
 def begins_with_r(array)
   if array.all? do |element|
@@ -47,11 +49,76 @@ def remove_non_strings(array)
   return array2
 end
 
+
 def count_elements(array)
-  count_arr = []
-  count_hash = {}
-  counter = 0
-  array.each do |key, value|
-    
+  #count_arr = []
+  #count_hash = Hash.new(0)
+  #counter = 0
+  array.each do |hash|
+    hash[:count] = 0
+    name = hash[:name]
+    array.each do |count_hash|
+      if count_hash[:name] == name
+        hash[:count] += 1
+      end
+    end
+    #hash.each do |key, value|
+      #count_hash[key] = value
+      #count_hash[:count] += 1
+      #count_arr[counter] = count_hash
+      #count_arr[counter] = (count_hash[key] = value)
+      #count_arr[counter] = (count_hash[:count] += 1)
+      #counter += 1
+    #end
   end
+  return array.uniq
 end
+#result --> [{:name=>"blake", :count=>2}, {:name=>"ashley", :count=>1}]
+
+def merge_data(keys, data)
+  #merge_arr = []...?
+  keys.each do |person|
+    data.each do |name|
+      name.each do |details, detail|
+        #merge_arr << person.merge(detail)...?
+        merged_data = person.merge(detail)
+      end
+    end
+  end
+  return merged_data.uniq #merge_arr.uniq...?
+end
+#result --> [{:first_name=>"blake", :awesomeness=>10, :height=>"74",
+# :last_name=>"johnson"}, {:first_name=>"ashley", :awesomeness=>9,
+# :height=>60, :last_name=>"dubs"}]
+
+def find_cool(cool)
+  cool_arr = []
+  cool_hash = {}
+  cool.each do |list|
+    list.each do |person, temp|
+      if temp == "cool"
+        cool_arr << list
+        #cool_hash[person] = temp
+      end
+    end
+  end
+  return cool_arr #cool_hash
+end
+#result --> [{:name=>"blake", :temperature=>"cool"}]
+
+def organize_schools(schools)
+  organized_schools = {}
+  schools.each do |name, schools|
+    schools.each do |school, location|
+      if organized_schools.include?(location)
+        organized_schools[location] << name
+      else
+        organized_schools[location] = []
+        organized_schools[location] << name
+      end
+    end
+  end
+  return organized_schools
+end
+#result --> {"NYC"=>["flatiron school bk", "flatiron school", "general assembly"],
+# "SF"=>["dev boot camp", "Hack Reactor"], "Chicago"=>["dev boot camp chicago"]}
